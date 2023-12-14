c681be7a2fba1fd2ea4de879612ca00f9de2b475
psytron <m@psytron.com>
2023-12-13 18:35:59 -0800

    Update pcf.py

--- pcf.py
+++ pcf.py
@@ -1,6 +1,8 @@
 import time
 import board
 
+import subprocess
+
 import adafruit_pcf8591.pcf8591 as PCF
 from adafruit_pcf8591.analog_in import AnalogIn
 from adafruit_pcf8591.analog_out import AnalogOut
@@ -17,21 +19,23 @@ from adafruit_pcf8591.analog_out import AnalogOut
 #
 #####################################################################
 i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+#i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 pcf = PCF.PCF8591(i2c)
 
 pcf_in_0 = AnalogIn(pcf, PCF.A0)
+pcf_in_1 = AnalogIn(pcf, PCF.A1)
 pcf_out = AnalogOut(pcf, PCF.OUT)
 
+print( 'booting: ')
+print( PCF.A0 , PCF.OUT , PCF )
 while True:
-    print("Setting out to ", 65535)
+    print( ' reference voltage: ', pcf_in_0.reference_voltage )
     pcf_out.value = 65535
-    raw_value = pcf_in_0.value
+    raw_value = pcf_in_1.value
     scaled_value = (raw_value / 65535) * pcf_in_0.reference_voltage
 
-    print("Pin 0: %0.2fV" % (scaled_value))
-    print("")
-    time.sleep(1)
+    print("pcf_in_1_scaled:", scaled_value )
+    print( 'pcf_in_1_raw: ',pcf_in_1.value ) 
 
     print("Setting out to ", 32767)
     pcf_out.value = 32767
@@ -39,14 +43,14 @@ while True:
     scaled_value = (raw_value / 65535) * pcf_in_0.reference_voltage
 
     print("Pin 0: %0.2fV" % (scaled_value))
-    print("")
-    time.sleep(1)
-
-    print("Setting out to ", 0)
+    #print("")
+    #print("Setting out to ", 0)
     pcf_out.value = 0
     raw_value = pcf_in_0.value
     scaled_value = (raw_value / 65535) * pcf_in_0.reference_voltage
 
-    print("Pin 0: %0.2fV" % (scaled_value))
-    print("")
-    time.sleep(1)
+    #print("Pin 0: %0.2fV" % (scaled_value))
+    
+    
+    
+    print("\033c", end="")
