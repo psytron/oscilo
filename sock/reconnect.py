69f492368907e253fdbd8a022cea3226f7622048
Psy Tron <m@psytron.com>
2023-12-29 18:24:51 -0800

    wip

--- sock/reconnect.py
+++ sock/reconnect.py
@@ -38,3 +38,39 @@ finally:
     GPIO.cleanup()
 
 
+
+# The IC with marking N18 355T W is likely a temperature sensor with digital output (NTC) and digital input (DIN).
+# To connect the GPIO to it, we need to set up the NTC and DIN pins as input pins.
+# We have already done this above with the lines:
+# GPIO.setup(NTC_PIN, GPIO.IN)
+# GPIO.setup(DIN_PIN, GPIO.IN)
+# Now, we can read the values from these pins as shown above.
+# If you want to do something based on the values read, you can add your code here.
+# For example, if the NTC value is above a certain threshold, you might want to turn on a fan.
+# You can do this by setting up another GPIO pin as an output pin, and turning it on or off based on the NTC value.
+# Here is an example of how you might do this:
+
+FAN_PIN = 27  # Replace with the actual GPIO number you connected to the fan
+GPIO.setup(FAN_PIN, GPIO.OUT)
+
+try:
+    while True:
+        # Read NTC value
+        ntc_value = GPIO.input(NTC_PIN)
+        print(f"NTC Value: {ntc_value}")
+
+        # If the NTC value is above a certain threshold, turn on the fan
+        if ntc_value > 50:  # Replace 50 with the actual threshold you want to use
+            GPIO.output(FAN_PIN, GPIO.HIGH)
+        else:
+            GPIO.output(FAN_PIN, GPIO.LOW)
+
+        time.sleep(1)  # Wait for 1 second before reading again
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    # Clean up GPIO settings
+    GPIO.cleanup()
+
---
+++ sock/smbus_pcf.py
@@ -0,0 +1,19 @@
+
+
+import smbus
+import time
+
+# I2C bus address (adjust if necessary for your setup)
+PCF8591_ADDRESS = 0x48
+
+# Register addresses
+PCF8591_ADC_REG = 0x00
+
+# Create I2C bus object
+bus = smbus.SMBus(1)  # Assuming I2C bus 1 is used
+
+# Continuously read and print analog values
+while True:
+    data = bus.read_byte_data(PCF8591_ADDRESS, PCF8591_ADC_REG)
+    print("Analog value:", data)
+    time.sleep(0.5)  # Delay between readings (adjust as needed)
