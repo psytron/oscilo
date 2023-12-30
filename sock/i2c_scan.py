0e4eb135eae63212bd4e577bfd3a5a0c69987b29
Psy Tron <m@psytron.com>
2023-12-29 17:43:09 -0800

    wip

---
+++ sock/i2c_scan.py
@@ -0,0 +1,20 @@
+
+
+
+import smbus
+
+# Create an SMBus object
+bus = smbus.SMBus(1)  # Use 1 for Raspberry Pi 3, 0 for Raspberry Pi 1 or 2
+
+try:
+    for i in range(128):
+        # Try to read from each possible I2C address
+        bus.read_byte(i)
+        print(f"Device found at address 0x{i:02X}")
+
+except IOError:
+    pass  # No device at this address
+
+finally:
+    # Clean up resources
+    bus.close()
