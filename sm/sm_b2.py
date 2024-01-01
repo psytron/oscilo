1cd93fbe220099a40f92737a5263222d2f6fcada
Psy Tron <m@psytron.com>
2024-01-01 09:20:22 -0800

    regs

--- sm/sm_b2.py
+++ sm/sm_b2.py
@@ -5,6 +5,11 @@ from smbus2 import SMBus
 
 # Open i2c bus 1 and read one byte from address 80, offset 0
 bus = SMBus(1)
+# The function read_byte_data is used to read a single byte from a device at a specific address.
+# The first parameter '80' is the address of the device on the I2C bus.
+ # The second parameter '0' is the register on the device to read from.
+ # A register is a small amount of storage available on the I2C device. 
+ # The number of registers varies per device, but each register has a unique address.
 b = bus.read_byte_data(80, 0)
 print(b)
 bus.close()
