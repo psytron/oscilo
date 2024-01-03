e38b673ed1aec32d5512ee300df1960acf5f8a5a
Psy Tron <m@psytron.com>
2024-01-03 11:23:55 -0800

    wip

--- screen_t1.py
+++ screen_t1.py
@@ -10,6 +10,20 @@ lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=16, rows=2, dot
               backlight_enabled=True)
 
 # Write a string to the LCD
+lcd.write_string('0000 ')
+sleep(1)
+lcd.write_string('0001 ')
+sleep(1)
+lcd.write_string('0002 ')
+sleep(1)
+lcd.write_string('0003 ')
+sleep(1)
+lcd.write_string('0004 ')
+sleep(1)
+lcd.write_string('0005 ')
+sleep(1)
+
+
 lcd.write_string('VOLT DISPLAY ')
 
 sleep(1 )
