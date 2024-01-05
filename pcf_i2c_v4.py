df6d9e85a353521c959ab47fdbed90dd241c01ec
Psy Tron <m@psytron.com>
2024-01-05 12:48:33 -0800

    wip

--- pcf_i2c_v4.py
+++ pcf_i2c_v4.py
@@ -7,31 +7,35 @@ bus = SMBus(1)  # 1 indicates /dev/i2c-1
 address = 0x48  # replace with your device address
 command_A0 = 0x40  # control command for analog pin A0 
 command_A1 = 0x41  # control command for analog pin A1 
-mesg_df = pd.DataFrame(columns=['A0', 'A1'])
+df = pd.DataFrame(columns=['A0', 'A1'])
+df['A0'] = [0]*30
+df['A1'] = [0]*30
+
 
 while True:
-    # Read from A0
-    time.sleep(0.1)  #
-    bus.write_byte(address, command_A0)
-    time.sleep(0.1)  # wait for ADC to complete
-    value_A0 = bus.read_byte(address)
-    mesg_df = mesg_df.append({'A0': value_A0}, ignore_index=True)
-    print("A0 value:", value_A0 )
-    print( asciichartpy.plot ( mesg_df['A0'].tolist()  , {"height":10} ) )
-    mesg_df = mesg_df.iloc[1:]
-
-    # Read from A1
-    time.sleep(0.1)  #
-    bus.write_byte(address, command_A1)
-    time.sleep(0.1)  # wait for ADC to complete
+    # READ A0
+    bus.write_byte( address, command_A0 )
+    time.sleep(0.1)  # AWAIT ADC 
+    value_A0 = bus.read_byte( address )
+    
+    # READ A1
+    bus.write_byte( address, command_A1 )
+    time.sleep(0.1)  # AWAIT ADC 
     value_A1 = bus.read_byte(address)
-    mesg_df = mesg_df.append({'A1': value_A1}, ignore_index=True)
-    print("A1 value:", value_A1 )
-    print( asciichartpy.plot ( mesg_df['A1'].tolist()  , {"height":10,"colors":[asciichartpy.red]} ) )
-    mesg_df = mesg_df.iloc[1:]
+
+    # ADD DATA ROWS 
+    new_rows = [ { 'A0': value_A0, 'A1': value_A1 } ]
+    df = df.append(new_rows, ignore_index=True)
+
+    # DISPLAY DF 
+    print( "A0 value:", value_A0 )
+    print( asciichartpy.plot ( df['A0'].tolist()  , {"height":10} ) )
+    print( "A1 value:", value_A1 )
+    print( asciichartpy.plot ( df['A1'].tolist()  , {"height":10,"colors":[asciichartpy.red]} ) )
     
+    # TRUNC DF 
+    df =df.iloc[1:]
     
-    
-    # reset screen 
+    # RESET SCREEN
     print("\033c", end="")
     time.sleep(0.05)
