d4c0b6db66f3e34fc31b5010685edcc64756476f
Psy Tron <m@psytron.com>
2024-01-26 15:05:08 -0800

    wip

--- ads1256/harvest.py
+++ ads1256/harvest.py
@@ -20,13 +20,13 @@ try:
 
     while(1):
         ADC_Value = ADC.ADS1256_GetAll()
-        print ("0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff))
-        print ("1 ADC = %lf"%(ADC_Value[1]*5.0/0x7fffff))
-        print ("2 ADC = %lf"%(ADC_Value[2]*5.0/0x7fffff))
-        print ("3 ADC = %lf"%(ADC_Value[3]*5.0/0x7fffff))
-        print ("4 ADC = %lf"%(ADC_Value[4]*5.0/0x7fffff))
-        print ("5 ADC = %lf"%(ADC_Value[5]*5.0/0x7fffff))
-        print ("6 ADC = %lf"%(ADC_Value[6]*5.0/0x7fffff))
+        print ("~0 ADC = %lf"%(ADC_Value[0]*5.0/0x7fffff))
+        print ("~1 ADC = %lf"%(ADC_Value[1]*5.0/0x7fffff))
+        print ("~2 ADC = %lf"%(ADC_Value[2]*5.0/0x7fffff))
+        print (" 3 ADC = %lf"%(ADC_Value[3]*5.0/0x7fffff))
+        print (" 4 ADC = %lf"%(ADC_Value[4]*5.0/0x7fffff))
+        print (" 5 ADC = %lf"%(ADC_Value[5]*5.0/0x7fffff))
+        print (" 6 ADC = %lf"%(ADC_Value[6]*5.0/0x7fffff))
         #print ("7 ADC = %lf"%(ADC_Value[7]*5.0/0x7fffff))
         
         print("RND    = %d" % random.randint(0, 100))
