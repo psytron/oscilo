fc38a7d19b5f7648f07627fba5e321b2a4238a61
Psy Tron <m@psytron.com>
2024-01-26 17:29:48 -0800

    wip

--- sharedmemy6.py
+++ sharedmemy6.py
@@ -51,7 +51,7 @@ def worker( evnt ):
     while True: 
         i=9
         print( 'MTRX: ', mtrx , random.randint(1, 100) )
-        stream.write( generate_tone( mtrx[0], 0.03 ) )        
+        stream.write( generate_tone( 0.01 , matrix[0] ) )        
 
 
 
