3635b19af3fba53f0a8588564f99624906bdfb2e
psytron <m@psytron.com>
2024-01-29 04:07:44 -0800

    update raw array and mmap

---
+++ capacitanc.py
@@ -0,0 +1,38 @@
+
+
+# 1M Ohm resistor
+# Jumper wires
+# wire antenna
+
+# Connect one end of the resistor to a GPIO pin on the Raspberry Pi (let's say GPIO 4).
+# Connect the other end of the resistor to the antenna (the piece of wire).
+# Connect the antenna to a ground pin on the Raspberry Pi through a jumper wire.
+
+
+import RPi.GPIO as GPIO
+import time
+
+sensorPin = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(sensorPin, GPIO.OUT)
+
+def readCapacitance():
+    GPIO.output(sensorPin, GPIO.HIGH)
+    time.sleep(0.1)
+    GPIO.setup(sensorPin, GPIO.IN)
+    start_time = time.time()
+    end_time = time.time()
+    while GPIO.input(sensorPin) == GPIO.HIGH:
+        end_time = time.time()
+        if end_time - start_time > 1:
+            return False
+    return end_time - start_time
+
+while True:
+    capacitance = readCapacitance()
+    if capacitance:
+        print("Capacitance: ", capacitance)
+    else:
+        print("Timeout")
+    time.sleep(0.1)
