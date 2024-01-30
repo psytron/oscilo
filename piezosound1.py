de51522ef5aba18de6f3b80e77183eaf4623f679
Psy Tron <m@psytron.com>
2024-01-30 09:29:59 -0800

    wip

--- piezosound1.py
+++ piezosound1.py
@@ -23,12 +23,16 @@ def play_tone(pwm, note, duration):
         pwm.ChangeFrequency(440 * 2 ** (i / 12))  # Change the frequency
         time.sleep(duration / len(note))  # Wait for the note to end
 
+# This line initializes a PWM (Pulse Width Modulation) instance on the GPIO pin connected to the buzzer.
+# The PWM instance is used to control the frequency of the electrical signal sent to the buzzer, which in turn controls the sound produced by the buzzer.
+# The first argument, 'buzzerPin', is the GPIO pin number to which the buzzer is connected.
+# The second argument, '1', is the initial frequency of the PWM signal in Hertz. This frequency will be changed later in the code to produce different tones.
 pwm = GPIO.PWM(buzzerPin, 1)
 
 try:
     pwm.start(1)
     for i in range(-12, 13):
-        play_tone(pwm, generate_tone(440 * 2 ** (i / 12), 1), 1)
+        play_tone(pwm, generate_tone(240 * 2 ** (i / 12), 1), 1)
 finally:
     pwm.stop()
     GPIO.cleanup()
