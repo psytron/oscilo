7e0c2ecef5b5a61c685d2d01ff2d122e7515a74a
Psy Tron <m@psytron.com>
2024-02-01 22:41:29 -0800

    wip

--- gpiod_ada2.py
+++ gpiod_ada2.py
@@ -1,8 +1,11 @@
 import time
 import gpiod
 from gpiod.line import Direction, Value
+import numpy as np
 
 LINE = 26
+
+delay = 0.001
 
 with gpiod.request_lines(
     "/dev/gpiochip4",
@@ -13,8 +16,16 @@ with gpiod.request_lines(
         )
     },
 ) as request:
+    start_time = time.time()
     while True:
         request.set_value(LINE, Value.ACTIVE)
-        time.sleep(0.01)
+        time.sleep( delay )
         request.set_value(LINE, Value.INACTIVE)
-        time.sleep(0.01)
+        time.sleep( delay )
+        
+        elapsed_time = time.time() - start_time
+        delay = 0.05 * (1 + np.sin(2 * np.pi * elapsed_time / 10))
+        request.set_value(LINE, Value.ACTIVE)
+        time.sleep(delay)
+        request.set_value(LINE, Value.INACTIVE)
+        time.sleep(delay)
