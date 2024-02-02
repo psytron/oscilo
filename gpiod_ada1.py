1589806b6547e4477a6331b87c0190f54c6f0bf8
Psy Tron <m@psytron.com>
2024-02-01 22:29:09 -0800

    wip

--- gpiod_ada1.py
+++ gpiod_ada1.py
@@ -4,8 +4,8 @@
 
 import gpiod
 
-chip = gpiod.Chip('gpiochip4', gpiod.Chip.OPEN_BY_NAME)
-line = chip.get_line(18)
+chip = gpiod.Chip('gpiochip4')
+line = chip.get_line(26)
 line.request(consumer="blinktest", type=gpiod.LINE_REQ_DIR_OUT)
 
 while True:
