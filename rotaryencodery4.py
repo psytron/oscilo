04f5955014040fd666af27655bd6b87ac1c9c8fd
Psy Tron <m@psytron.com>
2024-02-02 16:27:32 -0800

    wip

---
+++ rotaryencodery4.py
@@ -0,0 +1,70 @@
+
+
+
+
+import RPi.GPIO as GPIO
+import time
+
+# Pin configuration
+# DEFAULT: 
+
+
+
+def setup_rotary_listener( CLK = 16  , DT = 20, SW = 21 , callback_in=print  ):
+    #CLK = 16  # Clock pin
+    #DT = 20  # Data pin
+    #SW = 21  # Switch pin   
+
+    # Variables
+    px = 0  # Initial value of px
+    last_CLK_state = False
+
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(SW, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(DT, GPIO.IN)
+    GPIO.setup(CLK, GPIO.IN)
+
+    def handle_CLK_edge(channel):
+        nonlocal last_CLK_state
+        nonlocal px
+        current_CLK_state = GPIO.input(CLK)
+        if current_CLK_state != last_CLK_state:
+            if current_CLK_state and GPIO.input(DT) != current_CLK_state:
+                px += 1  # Increment px on clockwise rotation
+            elif current_CLK_state and GPIO.input(DT) == current_CLK_state:
+                px -= 1  # Decrement px on counterclockwise rotation
+        last_CLK_state = current_CLK_state
+
+
+    def handle_SW_press(channel):
+        print("Switch pressed!")
+
+
+    # Add event listener for CLK pin
+    GPIO.add_event_detect(CLK, GPIO.BOTH, callback=handle_CLK_edge)    
+    GPIO.add_event_detect(SW, GPIO.FALLING, callback=handle_SW_press)
+
+
+    try:
+        while True:
+            time.sleep(0.01) # CPU LIMIT
+            callback_in( px )
+    except KeyboardInterrupt:
+        GPIO.remove_event_detect(CLK)
+        GPIO.remove_event_detect(DT)
+        GPIO.remove_event_detect(SW)
+        GPIO.cleanup()
+
+
+
+
+
+if __name__ == "__main__":
+    def print_px_a(px):
+        print("A px value: ", px)
+    setup_rotary_listener( 16,20,21,print_px_a)
+
+    def print_px_b(px):
+        print("B px value: ", px)
+    setup_rotary_listener( 23,24,25,print_px_b)    
