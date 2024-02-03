4fc10ecd580bb933f51922aa90f20868b0e52d5e
Psy Tron <m@psytron.com>
2024-02-02 16:12:39 -0800

    wip

--- rotaryGreen1.py
+++ rotaryGreen1.py
@@ -1,35 +1,24 @@
-
 import gpiod
-from gpiod.line import Direction, Value
-import time
- 
+from gpiod.line import Direction, Value, Event
 
 CLK = 16  # Clock pin
 DT = 20  # Data pin
 SW = 21  # Switch pin   
 
-# Variables
-px = 0  # Initial value of px
-last_CLK_state = False
-
 chip = gpiod.Chip('/dev/gpiochip4')
-rqLines = gpiod.request_lines( "/dev/gpiochip4",consumer="blink-example",
-    config={
-        CLK: gpiod.LineSettings(direction=Direction.INPUT, output_value=Value.ACTIVE),
-        DT: gpiod.LineSettings(direction=Direction.INPUT, output_value=Value.ACTIVE),
-        SW: gpiod.LineSettings(direction=Direction.INPUT, output_value=Value.ACTIVE),
 
-})
+# Request lines
+lines = chip.get_lines([CLK, DT, SW])
+lines.request(consumer="blink-example", type=gpiod.LINE_REQ_EV_BOTH_EDGES)
 
+# Initial state
+last_CLK_state = lines[0].event_read().event_type
 
-last_CLK_state= rqLines.get_values()[0].value
-
-while True:
-    time.sleep(0.1) # CPU LIMIT
-    valz =  rqLines.get_values()
-
-    current_CLK_state = valz[0].value
-    current_DT_state = valz[1].value
+def event_callback(line):
+    global last_CLK_state
+    event = line.event_read()
+    current_CLK_state = event.event_type
+    current_DT_state = lines[1].get_value()
 
     if current_CLK_state != last_CLK_state:  # Means the knob is rotating
         # if the DT state is different than the CLK state, that means the knob is rotating clockwise (right)
@@ -40,3 +29,9 @@ while True:
 
     last_CLK_state = current_CLK_state  # Update the last CLK state
 
+# Register event callback
+lines[0].event_wait(sec=0)
+lines[0].event_config(type=gpiod.LINE_REQ_EV_BOTH_EDGES, callback=event_callback)
+
+while True:
+    time.sleep(0.1)  # CPU LIMIT
