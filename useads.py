fdcdb324bf8ff3a27cbed58ae81c68de4aee0b03
Psy Tron <m@psytron.com>
2024-02-21 15:56:40 -0800

    wip

--- useads.py
+++ useads.py
@@ -3,5 +3,10 @@
 
 import ads
 
-while True:
-    print( ads.read() )
+
+# uses loop to read 
+# while True:
+#     print( ads.read() )
+
+
+ads.stream_to_address_on_port('Alophants-Air.lan', 65432 )
