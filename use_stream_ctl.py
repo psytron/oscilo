0cf015611a8a15f105cb5ce9463ac765b32ace1b
Psy Tron <m@psytron.com>
2024-02-21 16:29:57 -0800

    wip

---
+++ use_stream_ctl.py
@@ -0,0 +1,18 @@
+
+
+
+import rotaryd
+import ads
+
+
+
+def rotary_update( x_in , y_in ):
+    print( x_in , y_in  )
+
+
+rotaryd.setup_rotary_listener( 16  , 20,  21 , 23 , 24 , 25 , rotary_update    )
+
+
+while True:
+    valz = ads.read()
+    print( valz )
