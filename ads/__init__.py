a1eb7c3cdcd94eb8853fe44c6f914ab2947448ab
Psy Tron <m@psytron.com>
2024-02-21 17:19:46 -0800

    wip

--- ads/__init__.py
+++ ads/__init__.py
@@ -5,5 +5,6 @@ from . import harvest
 
 print("Current file name: ", os.path.basename(__file__))
 read = harvest.read
+readzero = harvest.readzero
 stream_to_address_on_port = harvest.stream_to_address_on_port
 
