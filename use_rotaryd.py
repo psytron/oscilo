cae541acb80e7b80c33f4750823cc5d3cbf64f3b
Psy Tron <m@psytron.com>
2024-02-21 16:24:15 -0800

    wip

--- use_rotaryd.py
+++ use_rotaryd.py
@@ -5,8 +5,8 @@ import rotaryd
 
 
 
-def rotary_update( par_in ):
-    print( par_in )
+def rotary_update( x_in , y_in ):
+    print( x_in , y_in  )
 
 
 rotaryd.setup_rotary_listener( 16  , 20,  21 , 23 , 24 , 25 , rotary_update    )
