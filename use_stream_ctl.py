aa8eba489e0b09309add759c75716fe955f8fbe9
Psy Tron <m@psytron.com>
2024-02-21 16:31:16 -0800

    wip

--- use_stream_ctl.py
+++ use_stream_ctl.py
@@ -10,7 +10,7 @@ def rotary_update( x_in , y_in ):
     print( x_in , y_in  )
 
 
-rotaryd.setup_rotary_listener( 16  , 20,  21 , 23 , 24 , 25 , rotary_update    )
+# rotaryd.setup_rotary_listener( 16  , 20,  21 , 23 , 24 , 25 , rotary_update    )
 
 
 while True:
