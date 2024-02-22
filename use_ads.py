4c7a482fa30ddf5e480c798bfcfa8a9377123612
Psy Tron <m@psytron.com>
2024-02-21 17:26:08 -0800

    wip

--- use_ads.py
+++ use_ads.py
@@ -5,7 +5,16 @@ import ads
 
 
 # uses loop to read 
+import time
+start_time = time.time()
+iterations = 0
 while True:
-     print( ads.readzero() )
+    #print(ads.readzero())
+    z = ads.readzero()
+    iterations += 1
+    if time.time() - start_time >= 1:
+        print(f"Iterations per second: {iterations}")
+        iterations = 0
+        start_time = time.time()
 
 #ads.stream_to_address_on_port('Alophants-Air.lan', 65432 )
